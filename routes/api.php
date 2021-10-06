4ed3d75e74b36b166ac6c252c5bf9daa97e7ef34
JERICO HERNANDO <jerico@interlink.com.ph>
2021-10-07 00:03:01 +0800

    changes on 10/07/2021

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,10 @@ use App\Http\Controllers\LocationController;
 |
 */
 
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 
 Route::get('/memberlocations', [App\Http\Controllers\LocationController::class, 'member']);
